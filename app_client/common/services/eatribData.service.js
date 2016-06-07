b488f7eac6b1c0d253252b375f0e8d6b2be15310
Alberto Quesada <qa.alberto@gmail.com>
2016-06-07 13:09:21 +0200

    Don't overlap body content and footer

--- app_client/common/services/eatribData.service.js
+++ app_client/common/services/eatribData.service.js
@@ -4,7 +4,7 @@ angular
 
 function eatribData ($http) {
   var locationByCoords = function(lat, lng) {
-    return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxdist=20');
+    return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxdist=2000000');
   };
   return {
     locationByCoords : locationByCoords
