b7eaaa1a42bdb5ead05b8956c337b83250c2216e
Alberto Quesada <qa.alberto@gmail.com>
2016-06-07 13:36:18 +0200

    Bug fix

--- app_client/common/services/eatribData.service.js
+++ app_client/common/services/eatribData.service.js
@@ -4,7 +4,7 @@
     .module('eatribApp')
     .service('eatribData', eatribData);
 
-  eatribData.$inject = ['http'];
+  eatribData.$inject = ['$http'];
   function eatribData ($http) {
     var locationByCoords = function(lat, lng) {
       return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxdist=2000000');
